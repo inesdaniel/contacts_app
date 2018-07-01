d1533841161618258ce82b2c5f55223792cbddac
Ines Daniel <i.danie8@gmail.com>
2018-07-01 11:25:02 -0500

    make update action

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -20,4 +20,15 @@ class Api::ContactsController < ApplicationController
     @contact1.save
     render "show.json.jbuilder"
   end
+
+  def update
+    contact_id = params[:id]
+    @contact1 = Contact.find_by(id:contact_id)
+    @contact1.first_name = params[:input_first_name] || @contact1.first_name
+    @contact1.last_name = params[:input_last_name] || @contact1.last_name
+    @contact1.email = params[:input_email] || @contact1.email
+    @contact1.phone_number = params[:input_phone_number] || @contact1.phone_number
+    @contact1.save
+    render "show.json.jbuilder"
+  end
 end
--- runner.rb
+++ runner.rb
@@ -2,14 +2,22 @@ require 'unirest'
 
 
 # create action 
-p "enter new first_name, last_name, email, phone_number:"
-response = Unirest.post("http://localhost:3000/api/contacts",
+# p "enter new first_name, last_name, email, phone_number:"
+# response = Unirest.post("http://localhost:3000/api/contacts",
+#   parameters: {
+#     first_name: gets.chomp, 
+#     last_name: gets.chomp, 
+#     email: gets.chomp,
+#     phone_number: gets.chomp
+#   }
+#   )
+
+# p response.body
+
+# update action
+response = Unirest.patch("http://localhost:3000/api/contacts/4",
   parameters: {
-    first_name: gets.chomp, 
-    last_name: gets.chomp, 
-    email: gets.chomp,
-    phone_number: gets.chomp
+    input_phone_number: "33349724"
   }
   )
-
 p response.body
