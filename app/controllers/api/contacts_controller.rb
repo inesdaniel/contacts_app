b82622bc73d1b369d3d60bb0456f4499751b835e
Ines Daniel <i.danie8@gmail.com>
2018-07-09 20:50:27 -0500

    add bio column with migrate file, update controller to show bio

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -13,7 +13,9 @@ class Api::ContactsController < ApplicationController
   def create
     @contact = Contact.new(
       first_name: params[:input_first_name], 
+      middle_name: params[:input_middle_name],
       last_name: params[:input_last_name], 
+      bio: params[:input_bio],
       email: params[:input_email],
       phone_number: params[:input_phone_number]
       )
@@ -25,7 +27,9 @@ class Api::ContactsController < ApplicationController
     contact_id = params[:id]
     @contact = Contact.find_by(id:contact_id)
     @contact.first_name = params[:input_first_name] || @contact.first_name
+    @contact.middle_name = params[:input_middle_name] || @contact.middle_name
     @contact.last_name = params[:input_last_name] || @contact.last_name
+    @contact.bio = params[:input_bio] || @contact.bio
     @contact.email = params[:input_email] || @contact.email
     @contact.phone_number = params[:input_phone_number] || @contact.phone_number
     @contact.save
--- app/views/api/contacts/_contact.json.jbuilder
+++ app/views/api/contacts/_contact.json.jbuilder
@@ -2,6 +2,7 @@ json.id contact.id
 json.first_name contact.first_name
 json.middle_name contact.middle_name
 json.last_name contact.last_name
+json.bio contact.bio
 json.email contact.email
 json.formatted do
   json.phone_number contact.japanese_code
